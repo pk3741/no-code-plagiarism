ef5c2b23dbbbd35e9ad5859c710d1478593ef1c9
pk3741 <przemek3741@gmail.com>
2022-12-28 19:27:49 +0100

    Fixes and optimalization

--- no-code-plagiarism/Command.cpp
+++ no-code-plagiarism/Command.cpp
@@ -3,9 +3,10 @@
 enum COMMAND_TYPE
 {
     //variable types
-    VAR_DECLARATION = 0,
-    VAR_INITIALIZATION = 1,
-    VAR_DEFINITION = 2  
+    UNDEFINED = 0,
+    VAR_DECLARATION = 1,
+    VAR_INITIALIZATION = 2,
+    VAR_DEFINITION = 3
 };
 
 Command::Command()
@@ -27,20 +28,28 @@ void Command::makeOfStr(std::string& str)
     check what we work with
     */
 
-    //VAR_DECLARATION
-    std::regex varDeclarationRegex("(\\w* \\w*)");
-    std::cout << "DECLARATION: " << std::regex_search(str, varDeclarationRegex) << std::endl;
-    commandType = VAR_DECLARATION;
-
-    //VAR_INITIALIZATION
-    std::regex varInitializationRegex("(\\w*=\\w*)");
-    std::cout << "INITIALIZATION: " << std::regex_search(str, varInitializationRegex) << std::endl;
-    commandType = VAR_INITIALIZATION;
-
     //VAR_DEFINITION
     std::regex varDefinitionRegex("(\\w* \\w*=\\w*)");
-    std::cout << "DEFINITION: " << std::regex_search(str, varDefinitionRegex) << std::endl;
-    commandType=VAR_DEFINITION;
+    bool isVarDefinition = std::regex_search(str, varDefinitionRegex);
+    
+    if (isVarDefinition)
+    {
+        commandType = COMMAND_TYPE::VAR_DEFINITION;
+    }
+    else
+    { 
+        //VAR_DECLARATION
+        std::regex varDeclarationRegex("(\\w* \\w*)");
+        bool isVarDeclaration = std::regex_search(str, varDefinitionRegex);
+        isVarDeclaration ? commandType = COMMAND_TYPE::VAR_DECLARATION : NULL;
+
+        //VAR_INITIALIZATION
+        std::regex varInitializationRegex("(\\w*=\\w*)");
+        bool isVarInitialization = std::regex_search(str, varInitializationRegex);
+        isVarInitialization ? commandType = COMMAND_TYPE::VAR_INITIALIZATION : NULL;
+    }
+    std::cout << commandType << std::endl;
+    
 
 
 
